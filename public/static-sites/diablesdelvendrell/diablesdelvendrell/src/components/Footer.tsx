918d15477619317777a1aacfb1222bdf3304a2a0
Joan Molinas <joan@atlabs.tech>
2024-10-22 21:06:39 +0200

    feat: main Add Diables del Vendrell html bridge

--- public/static-sites/diablesdelvendrell/diablesdelvendrell/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="text-center my-10 text-gray-200">
-      Powered by <span className="font-bold">Goil</span>
-    </div>
-  );
-};
-
-export default Footer;
--- public/static-sites/diablesdelvendrell/diablesdelvendrell/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div className="text-4xl font-bold text-center my-10 text-white">
-      Diables de El Vendrell
-    </div>
-  );
-};
-
-export default Header;
